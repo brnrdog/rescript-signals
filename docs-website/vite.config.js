975185ce68178399e41f5df79e3aed6788ca54b9
Bernardo Gurgel <brnrdog@hey.com>
2026-03-14 01:12:43 +0100

    docs: enable SSR prerendering for docs website (#15)

--- docs-website/vite.config.js
+++ docs-website/vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite"
 export default defineConfig({
   base: "/rescript-signals/",
   build: {
-    outDir: "dist",
+    outDir: "build/client",
     emptyOutDir: true,
   },
   server: {
